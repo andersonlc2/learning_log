a02c55f3754bf3975f20d5b9a1e826380eba4724
Anderson <acorreia610@gmail.com>
2021-01-19 19:34:07 -0300

    first commit

---
+++ learning_logs/urls.py
@@ -0,0 +1,21 @@
+"""Define padrões de URL para learning_logs."""
+
+from django.urls import path
+
+
+from . import views
+
+
+urlpatterns = [
+    # Página Inicial
+    path('', views.index, name='index'),
+
+    # Mostra todos os assuntos
+    path('topics/', views.topics, name='topics'),
+
+    # Página de detalhes para um único assunto
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    # Página para adicionar um novo assunto
+    path('new_topic/', views.new_topic, name='new_topic'),
+]
